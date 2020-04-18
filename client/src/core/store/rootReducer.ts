744cb112fa1bff70de5880b2e3b2432e27b2f6d5
Danijel Jambrečina <danijel.jambrecina@axilis.com>
2020-04-18 13:29:46 +0200

    Add redux-form reducer

--- client/src/core/store/rootReducer.ts
+++ client/src/core/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import playersReducer from './players/reducers';
 
 const rootReducer = combineReducers({
+  formReducer,
   players: playersReducer
 });
 
