eb376f81fa742448f30d60cdaa9ce13512d16729
Danijel Jambrečina <danijel.jambrecina@axilis.com>
2020-04-18 09:52:45 +0200

    Add home route

--- client/src/app/components/Navigation.tsx
+++ client/src/app/components/Navigation.tsx
@@ -5,6 +5,11 @@ import { Link } from 'react-router-dom';
 
 const Navigation: React.FunctionComponent = () => (
   <Menu mode="horizontal">
+    <Menu.Item key="home">
+      <Link to={paths.home}>
+        Tennly
+      </Link>
+    </Menu.Item>
     <Menu.Item key="leagues">
       <Link to={paths.leagues}>
         Leagues
