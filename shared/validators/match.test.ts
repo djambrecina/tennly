f3f337b60226ab93d51fd03b9c16b477e44cf688
Danijel Jambrečina <danijel.jambrecina@axilis.com>
2020-05-03 12:12:02 +0200

    Add more tests

--- shared/validators/match.test.ts
+++ shared/validators/match.test.ts
@@ -1,7 +1,35 @@
 import { isValidSetResult } from './match';
 
 describe("isValidSetResult", () => {
-  it("should return false when player1Games is 5", () => {
+  it("should return false when result is 5:0", () => {
     expect(isValidSetResult(5, 0)).toBe(false);
+  });
+
+  it("should return false when result is 5:4", () => {
+    expect(isValidSetResult(5, 4)).toBe(false);
+  });
+
+  it("should return false when result is 8:0", () => {
+    expect(isValidSetResult(8, 0)).toBe(false);
+  });
+
+  it("should return false when result is 8:6", () => {
+    expect(isValidSetResult(8, 6)).toBe(false);
+  });
+
+  it("should return false when result is 5:7", () => {
+    expect(isValidSetResult(5, 7)).toBe(true);
+  });
+
+  it("should return false when result is 6:7", () => {
+    expect(isValidSetResult(6, 7)).toBe(true);
+  });
+
+  it("should return false when result is 7:6", () => {
+    expect(isValidSetResult(7, 6)).toBe(true);
+  });
+
+  it("should return false when result is 7:5", () => {
+    expect(isValidSetResult(7, 5)).toBe(true);
   });
 });
