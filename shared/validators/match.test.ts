95d134dadf133385e05649a24ae16d1424dc2ea1
Danijel Jambrečina <danijel.jambrecina@axilis.com>
2020-05-03 12:17:14 +0200

    Add more tests

--- shared/validators/match.test.ts
+++ shared/validators/match.test.ts
@@ -1,12 +1,72 @@
 import { isValidSetResult } from './match';
 
 describe("isValidSetResult", () => {
+  it("should return true when result is 6:0", () => {
+    expect(isValidSetResult(6, 0)).toBe(true);
+  });
+
+  it("should return true when result is 6:1", () => {
+    expect(isValidSetResult(6, 1)).toBe(true);
+  });
+
+  it("should return true when result is 6:2", () => {
+    expect(isValidSetResult(6, 2)).toBe(true);
+  });
+
+  it("should return true when result is 6:3", () => {
+    expect(isValidSetResult(6, 3)).toBe(true);
+  });
+
+  it("should return true when result is 6:4", () => {
+    expect(isValidSetResult(6, 4)).toBe(true);
+  });
+
+  it("should return true when result is 7:5", () => {
+    expect(isValidSetResult(7, 5)).toBe(true);
+  });
+
+  it("should return true when result is 7:6", () => {
+    expect(isValidSetResult(7, 6)).toBe(true);
+  });
+
+  it("should return true when result is 0:6", () => {
+    expect(isValidSetResult(0, 6)).toBe(true);
+  });
+
+  it("should return true when result is 1:6", () => {
+    expect(isValidSetResult(1, 6)).toBe(true);
+  });
+
+  it("should return true when result is 2:6", () => {
+    expect(isValidSetResult(2, 6)).toBe(true);
+  });
+
+  it("should return true when result is 3:6", () => {
+    expect(isValidSetResult(3, 6)).toBe(true);
+  });
+
+  it("should return true when result is 4:6", () => {
+    expect(isValidSetResult(4, 6)).toBe(true);
+  });
+
+  it("should return true when result is 5:7", () => {
+    expect(isValidSetResult(5, 7)).toBe(true);
+  });
+
+  it("should return true when result is 6:7", () => {
+    expect(isValidSetResult(6, 7)).toBe(true);
+  });
+
   it("should return false when result is 5:0", () => {
     expect(isValidSetResult(5, 0)).toBe(false);
   });
 
   it("should return false when result is 5:4", () => {
     expect(isValidSetResult(5, 4)).toBe(false);
+  });
+
+  it("should return false when result is 6:5", () => {
+    expect(isValidSetResult(6, 5)).toBe(false);
   });
 
   it("should return false when result is 8:0", () => {
@@ -17,19 +77,23 @@ describe("isValidSetResult", () => {
     expect(isValidSetResult(8, 6)).toBe(false);
   });
 
-  it("should return false when result is 5:7", () => {
-    expect(isValidSetResult(5, 7)).toBe(true);
+  it("should return false when result is 0:5", () => {
+    expect(isValidSetResult(0, 5)).toBe(false);
   });
 
-  it("should return false when result is 6:7", () => {
-    expect(isValidSetResult(6, 7)).toBe(true);
+  it("should return false when result is 4:5", () => {
+    expect(isValidSetResult(4, 5)).toBe(false);
   });
 
-  it("should return false when result is 7:6", () => {
-    expect(isValidSetResult(7, 6)).toBe(true);
+  it("should return false when result is 5:6", () => {
+    expect(isValidSetResult(5, 6)).toBe(false);
   });
 
-  it("should return false when result is 7:5", () => {
-    expect(isValidSetResult(7, 5)).toBe(true);
+  it("should return false when result is 0:8", () => {
+    expect(isValidSetResult(0, 8)).toBe(false);
+  });
+
+  it("should return false when result is 6:8", () => {
+    expect(isValidSetResult(6, 8)).toBe(false);
   });
 });
